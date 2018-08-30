d1c6a1b25643cb1062d49643293112553f93c70d
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2018-08-30 11:02:25 +0200

    Lock before map update

--- hazelcast-node/src/main/java/test/hazelcast/HazelcastExampleService.java
+++ hazelcast-node/src/main/java/test/hazelcast/HazelcastExampleService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
 import com.hazelcast.core.IMap;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +20,18 @@ public class HazelcastExampleService {
 	@Scheduled(fixedRate = 5000)
 	public void updateDistributedCounter() {
 		
-		IMap<String, Integer> map = hazelcast.getMap("test");
+	    IMap<String, Integer> map = hazelcast.getMap("test");
+
+	    ILock lock = hazelcast.getLock("example");
+		lock.lock();
+		
 		if (!map.containsKey("counter")) {
 			map.put("counter", 1);
 		} else {
 			map.put("counter", map.get("counter") + 1);
 		}
+		
+		lock.unlock();
 		log.info("==== Distributed counter: {}", map.get("counter"));
 	}
 }
